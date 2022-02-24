96f1d030bcd0743b71c83020215f6126eba85071
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-24 12:35:50 +0330

    filtering data

--- movies.py
+++ movies.py
@@ -4,26 +4,11 @@ connection = sqlite3.connect('movies.db')
 
 cursor = connection.cursor()
 
-# cursor.execute("INSERT INTO Movies VALUES ('Taxi Driver', 'Martin Scorsese', 1976) ")
+release_year = (1985,)
 
-# cursor.execute("SELECT * FROM Movies")
+cursor.execute("SELECT * FROM Movies WHERE year=?", release_year)
 
-famousFilms = [('Pulp Fiction', 'Tarantino', 1994),
-               ('Back to the Future', 'Spielberg', 1985),
-               ('Moonrise Kingdom', 'Anderson', 2012)]
-# first insert data into Movies DB
-cursor.executemany('Insert INTO Movies VALUES (?,?,?)', famousFilms)
-
-# second call data from Movies DB and put in records variable
-records = cursor.execute("SELECT * FROM Movies")
-
-# use fetchall
 print(cursor.fetchall())
-
-# use for loop
-for record in records:
-    print(record)
-
 
 connection.commit()
 connection.close()
